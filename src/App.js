e5a9c435bc7f362aca78bc4c74d70fd2b4fc22dd
kuku20 <lluu3790@gmail.com>
2023-02-04 21:07:05 -0600

    header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   const team = [{ name: "Loc" }, { name: "Tung" }, { name: "Tay" }];
   return (
     <>
+      <div>
+        <Header />
+      </div>
       <div className="App">
         <h1>Hello Team</h1>
         <ul>
@@ -19,6 +22,21 @@ function App() {
 
 function Name({ member }) {
   return <li>{member.name}</li>;
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <div></div>
+      <div >
+        <img className="logoGoogle" src="https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1"></img>
+      </div>
+      <div>
+        
+        <input placeholder="search"></input>
+        </div>
+    </div>
+  )
 }
 
 export default App;
