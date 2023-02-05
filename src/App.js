ee3f43c173cf42454261e79ac05a43e491c1713b
Cong Nguyen <nguyencongkhai1407@gmail.com>
2023-02-04 21:13:31 -0600

    -banner design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
-  const team = [{ name: "Loc" }, { name: "Tung" }, { name: "Tay" }];
+  // const team = [{ name: "Loc" }, { name: "Tung" }, { name: "Tay" }];
   return (
     <>
-      <div className="App">
-        <h1>Hello Team</h1>
-        <ul>
-          {team.map((member) => (
-            <Name member={member} />
-          ))}
-        </ul>
-      </div>
+      {/* <div className="App">
+      </div> */}
+
+      {/* <ThemeProvider
+        breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+        minBreakpoint="xxs"
+      >
+
+      </ThemeProvider> */}
+
+      <Container>
+        {/* <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>2 of 3 (wider)</Col>
+
+      </Row> */}
+        <Row>
+          <Col>
+            <p><b>Min's Lash & Fibroblasts</b></p>
+            <p>
+              Open until 7:00 pm
+              •<a style="text-decoration: none" href="tel: +1 346-204-2210" >  1 346-204-2210</a>
+              •<a style="text-decoration: none" href="%202beautieslash@gmail.com" >  +  2beautieslash@gmail.com</a>
+
+            </p>
+            <p> At Min's Lash & Fibroblasts, we don’t strict the beauty in the same way. We prefer to bring out the best version of you instead. </p>
+          </Col>
+          <Col xs={4}>2 of 3 (wider)</Col>
+        </Row>
+      </Container>
     </>
   );
 }
