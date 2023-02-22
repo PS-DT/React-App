4164e45741a2abd2dbe155b7deb14d80edcacf67
CongNguyen <conghonguyen@utexas.edu>
2023-02-21 20:13:47 -0600

    Change buttonService left

--- src/App.js
+++ src/App.js
@@ -101,16 +101,17 @@ function StaffMember({ member }) {
 function Service() {
   return (
     <div className="service-card">
-      <div> 
-
-        <span style={{ fontWeight: 'bold' }}>Service Name </span>
-        <p><a href="#" className="service_button">Book me</a></p>
-      </div>
       <div>
-        <p  className="price_time"> $45.00 . 55 mins</p>
+        <span style={{ fontWeight: 'bold' }}>Service Name </span>
       </div>
-
-    </div>
+        <div className="price_time">
+            <p> $45.00 * 55 mins </p>
+        </div>
+        <div>
+          <p><a href="#" className="service_button">Book me</a></p>
+        </div>
+      </div>
   );
 }
+
 export default App;
